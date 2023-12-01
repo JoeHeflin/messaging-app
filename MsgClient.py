7a0c3e8bfdff1f282edc86fe6229dff1a1dfe6df
joeheflin <jnh24@duke.edu>
2023-12-01 00:10:04 +0000

    updated sock.send to match byte argument of python3

--- MsgClient.py
+++ MsgClient.py
@@ -1,27 +1,28 @@
 #Message Client (Python version 2.7)
-#Arguments: port #,
+#Arguments: address, port #
 
-import sys, socket, threading,select
+import sys, socket, threading, select, time
 
-host = 'localhost'
-port = int(sys.argv[1])
+privateLog = dict()
+
+host = sys.argv[1]
+port = int(sys.argv[2])
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = (host,port)
 pulse = True
+username = ""
 
 sock.connect(address)
 
 #Protocol: send prompts from server in same format as command line prompts
 
-def login(arg): #arg must be string
-	#print(arg)
+def login(arg): 
 	arg = arg.split(" ")
 	switcher = {
-		"PROMPT" : prompt,
-		"LOCKOUT" : userlockedout, #login <BLOCKED<> <BLOCKED_TIMER>
+		"userprompt" : userprompt,
+		"passprompt" : passprompt
 	}
 	command = arg[0]
-
 	try:	
 		argument = arg[1]
 		return switcher[command](argument)
@@ -29,54 +30,80 @@ def login(arg): #arg must be string
 		return switcher[command]()
 
 def keyboardSwitch(arg):
-	#print("Keyborad switch : "+arg)
 	switcher = {
 		"login" : login,
 		"sendmsg" : recvmsg,
-		# whoelse: whoelse(content)
-		# whoelsesince: whoelsesince(content)
-		# block: block(content)
-		# unblock: unblock(content)
-		"timeout": timeout,
-		"logout": logout
+		"logout": logout,
+		"startprivate" : startprivate,
+		"success" : success,
+		"private" : sendprivate,
+		"privaterequest" : privaterequest,
+		"private" : sendprivate,
+		"stopprivate" : stopprivate
 	}
-	#print("?????")
 	command = arg[0]
-
-	# print(command)
-	# print(arg[1:])
 	try:
-		#print(command + " SPACE " + content)
 		content = arg[1]
-	#print("?????")
-		#print("try "+ command + " " + content)
 		return switcher[command](content)
 	except:
 		return switcher[command]()
-	
-def timeout():
-	print("\nSession timeout due to inactivity")
-	logout()
 
-def prompt():
-	user = raw_input("Enter username: ")
-	code = raw_input("Enter password: ")
-	sock.send(user + " " + code)
-def userlockedout(timelocked):
-	print("Too many consecutive failed login attempts for this username, wait " + timelocked + " seconds to try again" )
-	global pulse
-	pulse = False
+def stopprivate(user):
+	privateLog[user].stop = True
+
+def privaterequest():
+	print("private connection requested")
+	try:
+		privateSocket.settimeout(5)
+		privateSocket.listen(1)
+	except:
+		print("timedout")
+	peerSocket, peerAddress = privateSocket.accept()
+	newP2P = privateThread(peerSocket)
+	newP2P.start()
+
+def success():
+	time.sleep(0.2)
+	addy, port = privateAddress
+	sock.send(("privateinfo "+ addy + " " + str(port)).encode())
+
+def sendprivate(usermsg):
+	user,msg = usermsg.split(" ",1)
+	if user in privateLog:
+		privateLog[user].sock.send(username+"(private): "+msg+"\z".encode())
+	else:
+		print("User not in private session. Use command \"startprivate <user>\" to start a private conversation")
+
+def startprivate(portandaddress):
+	print("trying to start private connection")
+	temp = portandaddress.split(" ")
+	julia = (temp[0],int(temp[1]))
+	time.sleep(0.002)
+	daniel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	daniel.connect(julia)
+	newP2P = privateThread(daniel)
+	newP2P.start()
+
+def userprompt():
+	global username
+	username = input("Enter username: ")
+	sock.send(username.encode())
+
+def passprompt():
+	password = input("Enter password: ")
+	sock.send(password.encode())
+
 def recvmsg(user):
 	print("\n"+user)
+
 def logout():
 	global pulse 
 	pulse = False
-
+	
 def messagercv():
 	return
-# def online():
-# def tryagain():
 
+# thread for keyboard inputs
 class keyThread (threading.Thread):
 	def __init__(self, arg):
 		threading.Thread.__init__(self)
@@ -85,9 +112,21 @@ class keyThread (threading.Thread):
 		while(pulse):
 			if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
 				readIn = sys.stdin.readline()[0:-1]
-				sock.send(readIn)
-		#print("exiting keyThread")
+				if readIn.split(" ")[0] == "private":
+					try:
+					#print("PRIVATE"+readIn)
+						keyboardSwitch(readIn.split(" ",1))
+					except:
+						print("Wrong format, try: private <user> <msg>")
+				elif readIn.split(" ")[0] == "stopprivate":
+					try:
+						keyboardSwitch(readIn.split(" ",1))
+					except:
+						print("Wrong format, try: stopprivate <user>")
+				else:
+					sock.send(readIn.encode())
 
+# thread to listen on server connectionf
 class serverThread (threading.Thread):
 	def __init__(self, arg):
 		threading.Thread.__init__(self)
@@ -95,16 +134,80 @@ class serverThread (threading.Thread):
 		sock.setblocking(0)
 	def run(self):
 		while(pulse):
+			sockBuffer = ""
 			try:
-				serverCommand = sock.recv(2048).split(" ",1)
-				#print(serverCommand)
-				keyboardSwitch(serverCommand)
+				# print("try")
+				data = sock.recv(2048).decode()
+				# print(data)
 			except:
+				data = ""
+			if data == "":
 				continue
+			else: #handles empty string
+				sockBuffer = sockBuffer + data
+				toDo = sockBuffer.split("\z")
+				for msg in toDo:
+					# print(msg)
+					if msg == "":
+						continue
+					else:
+						serverCommand = msg.split(" ",1)
+						keyboardSwitch(serverCommand)
 		sock.close()
-		#print("exiting serverThread")
 
+# Thread for listening to private connection
+class privateThread (threading.Thread):
+	def __init__(self, socket):
+		threading.Thread.__init__(self)
+		self.sock = socket
+		self.alive = True
+		self.stop = False
+		self.sock.setblocking(0)
+		self.peername = ""
+
+	def run(self):
+		self.sock.send("username "+username+"\z".encode())
+		while self.alive:
+			if not pulse or self.stop:
+				print("kill other thread")
+				self.sock.send("kill yourself".encode())
+				self.alive = False
+			else:
+				sockBuffer = ""
+				try:
+					data = self.sock.recv(2048).decode()
+				except:
+					data = ""
+				if data == "":
+					continue
+				else: #handles empty string
+					sockBuffer = sockBuffer + data
+					toDo = sockBuffer.split("\z")
+					for msg in toDo:
+						if msg == "":
+							continue
+						elif msg == "kill yourself":
+							self.alive = False
+						elif msg.split(" ")[0] == "username":
+							self.peername = msg.split(" ")[1]
+							privateLog[self.peername] = self
+						else:
+							print(msg)
+		try:
+			del privateLog[self.peername]
+		except:
+			print("Key error: ")
+			print(privateLog)
+		self.sock.close()
+		
+# Start client threads	
 serverConnect = serverThread("clientSocket")
 serverConnect.start()
 keyboardConnect = keyThread("clientKeyboard")
 keyboardConnect.start()
+
+# Build socket to listen for private connection but wait to be prompted to listen (line 58)
+temp = (host,0)
+privateSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+privateSocket.bind(temp)
+privateAddress = privateSocket.getsockname()
--- MsgServer.py
+++ MsgServer.py
@@ -1,22 +1,23 @@
 #Message Server (Python version 2.7)
-#Arguments: port #, BLOCK_DURATION, timeout
+#Arguments: port #, block duration, timeout length
 
 import sys, socket, threading, time
 
-offlinemsgs = dict()
-onlinelist = dict()
-lockedoutlist = dict()
-credlog = dict()
-lockLog = dict()
+offlinemsgs = dict()	#key = username, msgs recieved since last online
+onlinelist = dict()		#key = online username, ClientConnection object
+lockedoutlist = dict()	#key = username, value = ClientConnection object
+credlog = dict()		#key = valid username, value = valid associated password
+lockLog = dict()		#key = username, value = number of consecutive failed login attempts
+onlineHistory = dict() 	#key = username, value = time last seen
+blockedUsers = dict() 	#key = blocked person, value = list of people blocking them
 
 credfile = open("credentials.txt")
 
+# populate credlog
 for line in credfile:
 	creds = line.split(" ")
 	credlog[creds[0]] = creds[1].strip()
 	lockLog[creds[0]] = 0
-
-
 
 class ClientConnection(threading.Thread):
 	def __init__(self,clientSocket,clientAddress):
@@ -30,35 +31,44 @@ class ClientConnection(threading.Thread):
 
 
 	def run(self):
-
 		self.authorize()
 		self.sock.setblocking(0)
 		while(self.pulse):
+			# update onlineHistory for whoelsesice
+			if self.user in onlineHistory:
+				onlineHistory[self.user] = time.time()
 			timeDelta = time.time() - self.lastActive
 			timeLimit = float(sys.argv[3])
 			if(timeDelta > timeLimit):
-				self.sock.send("timeout")
+				self.sock.send("sendmsg timeout due to inactivity\z".encode())
 				print(self.user +" timed out")
 				self.clientSwitch(["logout"])
 				break
 			try:
-				clientCommand = self.sock.recv(2048).split(" ",1)
+				clientCommand = self.sock.recv(2048).decode().split(" ",1)
 				self.lastActive = time.time()
 				try:
 					self.clientSwitch(clientCommand)
 				except:
-					self.sock.send("sendmsg Invalid Command")
+					print(clientCommand)
+					self.sock.send("sendmsg Invalid Command\z".encode())
 			except:
 				continue
 		print("Exiting thread for "+self.user)
 		return
-		
+
 	def clientSwitch(self,arg):
 		command = arg[0]
 		switcher ={
 			"logout" : self.logout,
 			"message" : self.message,
-			"broadcast" : self.broadcast
+			"broadcast" : self.broadcast,
+			"whoelse" : self.whoelse,
+			"whoelsesince" : self.whoelsesince,
+			"block" : self.block,
+			"unblock" : self.unblock,
+			"startprivate" : self.startprivate,
+			"privateinfo" : self.privateinfo
 		}
 		try:
 			content = arg[1]
@@ -66,36 +76,112 @@ class ClientConnection(threading.Thread):
 		except:
 			return switcher[command]()
 
+	def privateinfo(self,addy):
+		temp = addy.split(" ")
+		onlinelist[self.user] = onlinelist[self.user],(temp[0],int(temp[1]))
+
+	def startprivate(self, user):
+		if(self.user in blockedUsers and destuser in blockedUsers[self.user]):
+				self.sock.send("sendmsg You can't start private messaging because the recipient has blocked you".encode())
+		elif user == self.user:
+			self.sock.send("sendmsg cannot start private message with self\z".encode())
+		elif user in onlinelist:
+			self.sock.send(("startprivate "+str(onlinelist[user][1][0])+" "+str(onlinelist[user][1][1])+"\z").encode())
+			onlinelist[user][0].sock.send("privaterequest".encode())	
+		elif user in credlog:
+			self.sock.send(("sendmsg "+user+" not online").encode())
+		else:
+			self.sock.send(("sendmsg "+user+" not a valid user").encode())
+			
+	def unblock(self,unblockeduser):
+		if unblockeduser in credlog:
+			if unblockeduser == self.user:
+				self.sock.send("sendmsg can't unblock yourself\z".encode())
+			elif unblockeduser in blockedUsers and self.user in blockedUsers[unblockeduser]:
+				blockedUsers[unblockeduser].remove(self.user)
+			else:
+				self.sock.send("sendmsg "+unblockeduser+" already unblocked\z".encode())
+			print("blocked users:")
+			print(blockedUsers)
+		else:
+			self.sock.send(("sendmsg "+unblockeduser+" doesn't exist\z").encode())
+
+	def block(self,blockeduser):
+		if blockeduser in credlog:
+			if blockeduser == self.user:
+				self.sock.send("sendmsg can't block yourself\z".encode())
+			elif blockeduser in blockedUsers and self.user in blockedUsers[blockeduser]:
+				self.sock.send(("sendmsg "+blockeduser+" already blocked\z").encode())
+			else:
+				if blockeduser in blockedUsers:
+					blockedUsers[blockeduser].append(self.user)
+				else:
+					blockedUsers[blockeduser] = [self.user]
+			print("blocked users:")
+			print(blockedUsers)
+		else:
+			self.sock.send(("sendmsg "+unblockeduser+" doesn't exist\z").encode())
+
+	def whoelsesince(self,timelen):
+		since = time.time() - float(timelen)
+		for person in onlineHistory:
+			if onlineHistory[person] > since and person != self.user:
+				self.sock.send(("sendmsg "+person+"\z").encode())
+
+	def whoelse(self):
+		for person in onlinelist:
+			if person != self.user:
+				self.sock.send(("sendmsg "+person+"\z").encode())
+
 	def broadcast(self,info):
 		for recipient in onlinelist:
 			if recipient != self.user:
-				self.message(recipient+" "+info)
-				
+				if(self.user in blockedUsers and recipient in blockedUsers[self.user]):
+					self.sock.send("sendmsg Your message wasn't delivered to some recipients".encode())
+				else:
+					self.message(recipient+" "+info+"\z")
+
 	def message(self,info):
 		destuser = info.split(" ")[0]
 		try:
 			msg = self.user + ": " + info.split(" ",1)[1]
-			if(destuser in credlog and destuser != self.user):
-				if(destuser in onlinelist):
-					onlinelist[destuser].sock.send("sendmsg "+ msg)
-				else:
-					if(destuser in offlinemsgs):
-						offlinemsgs[destuser] = offlinemsgs[destuser] +"\n"+msg
-					else:
-						offlinemsgs[destuser] = msg
+			if(self.user in blockedUsers and destuser in blockedUsers[self.user]):
+				self.sock.send("sendmsg Your message couldn't be delivered because the recipient has blocked you".encode())
 			else:
-				print("send error")
-				self.sock.send("sendmsg Invalid user: "+destuser)
-				return
+				if(destuser in credlog and destuser != self.user):
+					if(destuser in onlinelist):
+						onlinelist[destuser][0].sock.send(("sendmsg "+ msg+"\z").encode())
+					else:
+						if(destuser in offlinemsgs):
+							offlinemsgs[destuser] = offlinemsgs[destuser] +"\n"+msg
+						else:
+							offlinemsgs[destuser] = msg
+				else:
+					print("send error")
+					self.sock.send(("sendmsg Invalid user: "+destuser+"\z").encode())
+					return
 		except:
-			self.sock.send("sendmsg Invalid command format, try: message <user> <messgae>")
+			self.sock.send("sendmsg Invalid command format, try: message <user> <messgae>\z".encode())
 
 	def logout(self):
-		self.sock.send("logout")
-		del onlinelist[self.user]
-		for person in onlinelist:
-			onlinelist[person].sock.send("sendmsg "+self.user+" is no longer online")
+		# print("logout")
+		if self.user in onlinelist:
+			del onlinelist[self.user]
+			for person in onlinelist:
+				# print(person)
+				if(person not in blockedUsers):
+					# print("2")
+					# print(blockedUsers)
+					# print(person)
+					onlinelist[person][0].sock.send(("sendmsg "+self.user+" is no longer online\z").encode())
+					# print("3")
+				elif (self.user not in blockedUsers[person]):
+					onlinelist[person][0].sock.send(("sendmsg "+self.user+" is no longer online\z").encode())
+				# 	print("4")
+				# print("5")
+		# print("1")
 		self.pulse = False
+		self.sock.send("logout".encode())
 		print("User logged out, bye "+self.user)
 		return
 
@@ -103,67 +189,84 @@ class ClientConnection(threading.Thread):
 		
 		LOCK_DURATION = sys.argv[2]
 
-		self.sock.send("login PROMPT")
-		logininfo = self.sock.recv(2048)
-		try:
-			clientuser, clientpass = logininfo.split(" ")
-			print("recieved " + clientuser + " " + clientpass)
+		self.sock.send("login userprompt\z".encode())
+		logininfo = self.sock.recv(2048).decode()
+		if len(logininfo.split(" ")) == 1:
+			clientuser = logininfo
+			print("recieved " + clientuser)
 			self.user = clientuser
-			if(clientuser in onlinelist.keys()):
-				self.sock.send("sendmsg Account already online from a different address")
-				time.sleep(0.001)
-				self.authorize()
-			elif(clientuser in credlog.keys()):
-				if(lockLog[clientuser] == 2 or clientuser in lockedoutlist.keys()):
-					self.sock.send("login LOCKOUT " + LOCK_DURATION)
-					if(clientuser not in lockedoutlist.keys()):
-						print("client blocked")
-						lockedoutlist[clientuser] = self
-						time.sleep(float(LOCK_DURATION))
-						del lockedoutlist[clientuser]
-						lockLog[clientuser] = 0
-						self.pulse = False
-						print("locked out over for: " + clientuser)
-					return
-				elif (clientpass == credlog[clientuser]):
-					self.sock.send("sendmsg Login successful, Welcome!")
-					for user in onlinelist:
-						onlinelist[user].sock.send("sendmsg "+clientuser+" is now online")
-					onlinelist[clientuser] = self
-					if(self.user in offlinemsgs):
-						self.sock.send("sendmsg "+ offlinemsgs[self.user])
-						del offlinemsgs[self.user]
-					self.lastActive = time.time()
-					print(self.user+' logged in')
-					return 
-				else:
-					self.sock.send("sendmsg Wrong username or password, please try again")
-					lockLog[clientuser] = lockLog[clientuser] + 1
+			if clientuser in credlog.keys():
+				if(clientuser in onlinelist.keys()):
+					self.sock.send("sendmsg Account already online from a different address\z".encode())
 					time.sleep(0.001)
 					self.authorize()
+				else:
+					while 1:
+						if(lockLog[clientuser] == 3 or clientuser in lockedoutlist.keys()):
+							self.sock.send(("sendmsg locked out for "+str(LOCK_DURATION)+" seconds due to three consecutive failed attempts\z").encode())
+							print(self.user +" locked out")
+							if(clientuser not in lockedoutlist.keys()):
+								self.sock.send("logout".encode())
+								lockedoutlist[clientuser] = self
+								time.sleep(float(LOCK_DURATION))
+								del lockedoutlist[clientuser]
+								lockLog[clientuser] = 0
+								self.pulse = False
+								print("locked out over for: " + clientuser)
+							else:
+								self.clientSwitch(["logout"])
+							return
+						self.sock.send("login passprompt".encode())
+						passinfo = self.sock.recv(2048).decode()
+						if len(passinfo.split(" ")) == 1:
+							print("recieved "+ passinfo)
+							if (passinfo == credlog[clientuser]):
+								self.sock.send("sendmsg Login successful, Welcome!\z".encode())
+								for user in onlinelist:
+									print(onlinelist[user])
+									if(user not in blockedUsers):
+										onlinelist[user][0].sock.send(("sendmsg "+clientuser+" is now online\z").encode())
+									elif (self.user not in blockedUsers[user]):
+										onlinelist[user][0].sock.send(("sendmsg "+clientuser+" is now online\z").encode())
+								onlinelist[clientuser] = self
+								self.sock.send("success\z".encode())
+								onlineHistory[clientuser] = time.time()
+								if(self.user in offlinemsgs):
+									self.sock.send(("sendmsg "+ offlinemsgs[self.user]+"\z").encode())
+									del offlinemsgs[self.user]
+								self.lastActive = time.time()
+								print(self.user+' logged in')
+								return 
+							else:
+								self.sock.send("sendmsg  Invalid password. Please try again\z".encode())
+								lockLog[clientuser] = lockLog[clientuser] + 1
+								time.sleep(0.001)
+						else:
+							self.sock.send("sendmsg Invalid password. Please try again\z".encode())
+							lockLog[clientuser] = lockLog[clientuser] + 1
+							time.sleep(0.001)
 			else:	
-				self.sock.send("sendmsg Invalid Username")
+				self.sock.send("sendmsg Invalid Username. Please try again\z".encode())
 				time.sleep(0.001)
 				self.authorize()
-		except:
-			self.sock.send("sendmsg Invalid Username")
+		else:
+			self.sock.send("sendmsg Invalid Username. Please try again\z".encode())
 			time.sleep(0.001)
 			self.authorize()
 
+# build welcome socket
 port = int(sys.argv[1])
 address = ('localhost', port)
-
 welcomeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 welcomeSocket.bind(address)
 
+# create new socket connection and thread when requested
 while(1):
 	print("Waiting for New Client Connection")
 	welcomeSocket.listen(1)
 	clientSocket, clientAddress = welcomeSocket.accept()
 	newClient = ClientConnection(clientSocket,clientAddress)
 	newClient.start()
-
 	print("Starting New Thread")
 print('close')
 newClient.sock.close()
